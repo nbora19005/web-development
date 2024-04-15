c98cc3e97a1cc1276e2d8b43c1fb871d1318f9ad
nbora19005 <166815811+nbora19005@users.noreply.github.com>
2024-04-15 09:46:52 +0530

    Add files via upload

---
+++ day 5/script.js
@@ -0,0 +1,55 @@
+
+// var a=10
+// var b=23
+// console.log("addition-",a+b)
+// console.log("subtraction-",a-b)
+// console.log("multiplication-",a*b)
+// console.log("exponentiation-",a**b)
+// console.log("division",a/b)
+// console.log("modulus-",a%b)
+// console.log("increment",++b )
+// console.log(b)
+
+// console.log("increment",a++ )
+// console.log(a)
+// // console.log("decrement",--b )
+// let age=18
+// if(age<18){
+//     console.log("u are not adult")
+// }
+// else if (age>=55) {
+//     console.log("u are tooo old")
+    
+// } else {console.log("u are an adult")
+    
+// }
+
+// for(let i=2;i<=10;++i){
+//     console.log(i);
+// }            
+
+//   var a= "neha"
+//   var b="bora"
+//   var r=b.replace("bora","bisht")
+//   f=a.concat(r)
+//   console.log(f)
+  
+// var a=["hello","world"]
+
+// let colors=["pink","blue","black","white","aqua","neon","pink"]
+// let index=4
+// let col="grey"
+// console.log(colors[3])
+// colors.splice(index,2,col)
+// console.log(colors)
+
+function handlekeyup(){
+    let input = document.getElementById ("myinput")
+    let userinput = input.value;
+    console.log("user input" + userinput)
+}
+
+function handlesubmit(){
+    let from = document.getElementById("myform")
+    alert("from submitted")
+}
